5a0fdfb28939b4b07634fd65335fb4428ae7b633
Soumyadeeplogin <soumyadeeplogin@gmail.com>
2018-06-20 23:05:52 +0530

    20th

--- redux-basics.js
+++ redux-basics.js
@@ -6,6 +6,20 @@ const initialState = {
 }
 //Reducer
 const rootReducer = (state = initialState, action) => {
+    if(action.type==='INC_COUNTER')
+    {
+        return {
+            ...state,
+            counter: state.counter + 1
+        }
+    }
+    if(action.type==='ADD_COUNTER')
+    {
+        return {
+            ...state,
+            counter: state.counter + action.value
+        }
+    }
     return state
 }
 
@@ -13,7 +27,13 @@ const rootReducer = (state = initialState, action) => {
 const store= createStore(rootReducer)
 console.log(store.getState())
 
-//Dispatching Action
-
-
 //Subscription
+store.subscribe(()=>{
+    console.log('[Subscription]', store.getState())
+})
+
+//Dispatching Action
+store.dispatch({type: 'INC_COUNTER'})
+store.dispatch({type: 'ADD_COUNTER', value: 10})
+console.log(store.getState())
+
